b991d98eac18dbfcd0e33cea14d8faaa5e3f22e1
subaru-hello <octosubaru0926@gmail.com>
2022-12-08 12:33:25 +0900

    amplify init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "chatsample": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
